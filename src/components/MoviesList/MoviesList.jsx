825884a91a7533061e348e2e3b1282afe623f4c0
Tetiana Namestnykova <t.namestnykova@gmail.com>
2025-03-20 17:22:42 +0200

    solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -2,20 +2,24 @@ import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
 export const MoviesList = ({ movies, query }) => {
-  let visibleMovies = [...movies];
   const modifiedQuery = query.trim().toLowerCase();
+  let filteredMovies = movies;
 
   if (modifiedQuery) {
-    visibleMovies = visibleMovies.filter(
+    filteredMovies = movies.filter(
       movie =>
         movie.title.toLowerCase().includes(modifiedQuery) ||
-        movie.description.toLowerCase().includes(modifiedQuery),
+        movie.description.toLowerCase().includes(modifiedQuery)
     );
+  }
+
+  if (filteredMovies.length === 0) {
+    return <div className="movies">No movies found.</div>;
   }
 
   return (
     <div className="movies">
-      {visibleMovies.map(movie => (
+      {filteredMovies.map(movie => (
         <MovieCard key={movie.imdbId} movie={movie} />
       ))}
     </div>
